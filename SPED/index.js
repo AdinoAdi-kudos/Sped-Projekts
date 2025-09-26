d160718796d4524fe4261f5a7e6b26d4d1138907
Adi <162955723+AdinoAdi-kudos@users.noreply.github.com>
2025-09-26 17:07:41 +0800

    Add files via upload

---
+++ SPED/index.js
@@ -0,0 +1,17 @@
+// Javascript type shit
+console.log("Let's start pirating");
+alert("They ain't gonna stop us now twin");
+
+let name = "Autistic";
+console.log(name);
+
+// Button type shii
+document.addEventListener('DOMContentLoaded', function() {
+    const messageText = document.getElementByID('messageText');
+    const messageBox = document.getElementByID('messageBox');
+
+    //Button 1 - Hoever Effects pretty nice isn't it
+    const btn1 = document.getElementById('btn1');
+        messageText.textContent = "Hovering feels so good";
+        messageBox.style.background = "rgba (79, 172, 254, 0.2)";
+    });
